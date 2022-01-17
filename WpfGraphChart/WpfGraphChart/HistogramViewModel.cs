2d8e89363be5042483eb4822ff26798c0c397488
felixzhu1989 <zhu-hongfeng@hotmail.com>
2022-01-17 19:15:02 +0800

    直方图

---
+++ WpfGraphChart/WpfGraphChart/HistogramViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfGraphChart
+{
+    public class HistogramViewModel
+    {
+        public ObservableCollection<double> Values { get; set; } = new();
+
+        public HistogramViewModel()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                Values.Add(random.Next(10,90));
+            }
+        }
+    }
+}
