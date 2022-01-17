55dc83cbc6d77d75d84c6a99b0902bdc535ac562
felixzhu1989 <zhu-hongfeng@hotmail.com>
2022-01-17 20:18:26 +0800

    折线图

---
+++ WpfGraphChart/WpfGraphChart/PointsHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+
+namespace WpfGraphChart
+{
+    public class PointsHelper
+    {
+        //依赖附加属性
+        public static ObservableCollection<Point> GetPoints(DependencyObject obj)
+        {
+            return (ObservableCollection<Point>) obj.GetValue(PointsHelper.PointsProperty);
+        }
+        public static void SetPoints(DependencyObject obj, ObservableCollection<Point> value)
+        {
+            obj.SetValue(PointsProperty,value);
+        }
+        public static readonly DependencyProperty PointsProperty =
+            DependencyProperty.RegisterAttached("Points", typeof(ObservableCollection<Point>), typeof(PointsHelper), new PropertyMetadata(null,new PropertyChangedCallback(OnPointsChanged)));
+
+        private static void OnPointsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = d as Polyline;
+            var ps = e.NewValue as ObservableCollection<Point>;
+            ps.CollectionChanged += (se, ev) =>
+            {
+                obj.Points = new System.Windows.Media.PointCollection((e.NewValue as ObservableCollection<Point>)!);
+            };
+            
+        }
+    }
+}
---
+++ WpfGraphChart/WpfGraphChart/PolyLineViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfGraphChart
+{
+    public class PolyLineViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Point> Points { get; set; } = new();
+        public int OffsetX { get; set; } = 0;
+        public PolyLineViewModel()
+        {
+            Task.Run(async () =>
+            {
+                double angle = 0.0;
+                double x = 0.0;
+                while (angle < 360)
+                {
+                    await Task.Delay(50);
+                    double value = Math.Cos(Math.PI * angle / 180);
+                    angle += 5;
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        Points.Add(new Point(x, value * 40 + 50));
+                        x += 2;
+                        if (Points.Count > 150)
+                        {
+                            OffsetX -= 2;
+                            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OffsetX"));
+                            Points.RemoveAt(0);
+                        }
+                    });
+                    if (angle >= 360) 
+                        angle = 0.0;
+                }
+            });
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
